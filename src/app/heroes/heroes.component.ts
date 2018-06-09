ec7aa9c5fa9f72d79057b097abeb8b1f45578a45
uruha <kochmusica@gmail.com>
2018-06-09 15:43:08 +0900

    add heroes list select and style

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
